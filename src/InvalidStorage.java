c2bc83590cdc3a88f34d63de8bbcdaa95ed2bd79
artidotia <artidotia@gmail.com>
2016-07-09 10:44:52 +0530

    Session 1 Assignment 3 First commit
    Session 1 Assignment 3 First commit

---
+++ src/InvalidStorage.java
@@ -0,0 +1,12 @@
+
+public class InvalidStorage {
+
+	public static void main(String[] args) {
+/*		int num1;
+		num1 = "Hello"; //both the statements will return an error.
+		num1 = true;
+*/	
+		System.out.println("Example of Invalid storage");
+	}
+
+}
